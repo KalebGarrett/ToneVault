123632641f90c59ce8287bc23b6a6aad88c8c3c3
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-04 16:19:29 -0500

    Added scaffolding for identity

---
+++ ToneVault.Web/Areas/Identity/Data/ApplicationUserEntityConfig.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToneVault.Web.Areas.Identity.Data.ToneVault;
+
+namespace ToneVault.Web.Areas.Identity.Data;
+
+public class ApplicationUserEntityConfig : IEntityTypeConfiguration<WebUser>
+{
+    public void Configure(EntityTypeBuilder<WebUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+    }
+}
--- ToneVault.Web/Program.cs
+++ ToneVault.Web/Program.cs
@@ -1,5 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ToneVault.Web.Areas.Identity.Data;
+using ToneVault.Web.Areas.Identity.Data.ToneVault;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ToneGripDbContextConnection");
+builder.Services.AddDbContext<ToneGripDbContext>(options =>
+    options.UseSqlServer(connectionString));builder.Services.AddDefaultIdentity<WebUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ToneGripDbContext>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -17,11 +25,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
