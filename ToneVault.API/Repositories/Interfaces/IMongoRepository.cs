4624466a854cf1f4dd19e72ea583a875eb1ed517
Kaleb Garrett <kalebagarrett@gmail.com>
2024-01-17 20:40:08 -0500

    Created Mongo repo interface

---
+++ ToneVault.API/Repositories/Interfaces/IMongoRepository.cs
@@ -0,0 +1,13 @@
+﻿using ToneVault.Models;
+
+namespace ToneVault.API.Repositories.Interfaces;
+
+public interface IMongoRepository<T>  where T: BaseResource
+{
+    public Task<IEnumerable<T>> GetAll();
+    public Task<T> GetById(string id);
+    public Task<T> GetByOwnerId(string id);
+    public Task<T> Create(T data);
+    public Task<T> Update(string id, T data);
+    public Task<T> Delete(string id, bool hardDelete = false);
+}
