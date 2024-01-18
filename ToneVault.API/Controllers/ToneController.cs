314070a575cb0de6ea53c490037e8f6f730d246f
Kaleb Garrett <kalebagarrett@gmail.com>
2024-01-17 20:39:29 -0500

    Scaffolded controller for tones

---
+++ ToneVault.API/Controllers/ToneController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToneVault.Models;
+
+namespace ToneVault.API.Controllers;
+
+[ApiController]
+[Route("tones")]
+public class ToneController : ControllerBase
+{
+    [HttpGet("")]
+    public async Task<IActionResult> Get()
+    {
+        var tones = new List<Tone>();
+        tones.Add(new Tone());
+        return Ok(tones);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(string id)
+    {
+        return Ok(new Tone());
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> Create(Tone tone)
+    {
+        return Created("", tone);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, Tone tone)
+    {
+        return Ok(tone);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        return NoContent();
+    }
+}
