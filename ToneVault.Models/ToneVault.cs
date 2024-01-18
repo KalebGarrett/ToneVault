8fe7600525b737775f8eeee39b641fd664f80b9a
Kaleb Garrett <kalebagarrett@gmail.com>
2024-01-17 20:20:27 -0500

    Created Tone Vault model

---
+++ ToneVault.Models/ToneVault.cs
@@ -0,0 +1,14 @@
+﻿namespace ToneVault.Models;
+
+public class ToneVault : BaseResource
+{
+    public string Name { get; set; }
+    public string Genre { get; set; }
+    public int Bass { get; set; }
+    public int Middle { get; set; }
+    public int Treble { get; set; }
+    public int Gain { get; set; }
+    public int Reverb { get; set; }
+    public int Volume { get; set; }
+    public string Description { get; set; }
+}
