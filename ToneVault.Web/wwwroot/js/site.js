094d8ebb829ddc22c7183d0d04f35ef7c137fa19
Kaleb Garrett <kalebagarrett@gmail.com>
2024-03-06 18:07:31 -0500

    Added Google reCaptcha

--- ToneVault.Web/wwwroot/js/site.js
+++ ToneVault.Web/wwwroot/js/site.js
@@ -3,3 +3,15 @@
         lengthMenu: '_MENU_',
     },
 });
+
+function checkForm(){
+
+    if(!document.getElementById("g-recaptcha-response").value){
+        document.getElementById("reCaptcha-error").innerHTML =
+            "Please verify that you're not a robot!";
+        return false;
+    }else{
+        document.getElementById("submit-btn").type = "submit";
+        return true;
+    }
+}
